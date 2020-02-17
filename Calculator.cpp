7eb786712d46671470009919759eddb0005269a6
Rajil1213 <imrb1213@gmail.com>
2020-02-17 21:55:12 +0545

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class converter
+{
+private:
+    long int binary_whole;
+    long int binary_frac;
+
+};
+
+int main()
+{
+
+    return 0; //success
+}
